4e2ebdcd21466b66f9cb2a83fbb270ed53e64fda
Jono <jono@pulldigital.com>
2017-07-03 19:18:09 +0100

    Got MVC working with test action.

---
+++ jump.work-assignment/MVC Front End/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Web.Mvc;
+
+namespace jump.work.mvc.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return View("Index");
+        }
+        //[HttpGet]
+        //public ActionResult Reports()
+        //{
+        //    if( Request.QueryString["query"] != null)
+        //    {
+        //        return Reports(new ReportSearchResultsVM()
+        //        {
+        //            Query = Request.QueryString["query"],
+        //            IsGlobalSearch = true
+        //        });
+        //    }
+        //    else
+        //    {
+        //        return View("Index", GetResultsVM(SearchType.Reports));
+        //    }
+        //}
+        
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ jump.work-assignment/MVC Front End/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jump.work.mvc.MvcApplication" Language="C#" %>
